4a00ccd7f481953c0211dda20995800579976882
WSYAO7680 <wsy6988@qq.com>
2026-08-15 23:57:18 +0800

    彻底移除 L2 测试代码，保证 L1 纯净运行

--- starter_kit/adapter.py
+++ starter_kit/adapter.py
@@ -349,14 +349,3 @@ def agent_chat(prompt: str) -> str:
 
 def compile_hybrid(hybrid_qasm_str: str) -> tuple:
     raise NotImplementedError("L3 not implemented")
-
-qasm = agent_chat('''修改以下量子电路OPENQASM 2.0;
-include "qelib1.inc";
-qreg q[2];
-creg cx[2];
-h q[0];
-cx q[0], q[1];
-measure q -> c;''')
-print(qasm)
-#run_result = run(qasm, target="braket", shots=1024)
-#print(run_result)
